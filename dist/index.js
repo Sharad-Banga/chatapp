82aeccba52970dffdeb0f1f4f7de9828d406aa5b
Sharad-Banga <sharadiit03@gmail.com>
2025-10-25 21:37:38 +0530

    clearing chatapp

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+import { WebSocketServer } from "ws";
+const wss = new WebSocketServer({ port: 8081 });
+let c = 0;
+wss.on("connection", function (socket) {
+    c++;
+    console.log("user count is :" + c);
+    socket.on("message", function (e) {
+        console.log(e.toString());
+        socket.send(e.toString() + "sent from server");
+    });
+});
+//# sourceMappingURL=index.js.map
