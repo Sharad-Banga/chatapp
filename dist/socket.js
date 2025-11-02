7bdb33865fb4f51b5d4f53438f6878a73a4ae863
Sharad-Banga <sharadiit03@gmail.com>
2025-11-02 07:40:02 +0530

    learning websockets

---
+++ dist/socket.js
@@ -0,0 +1,29 @@
+import { WebSocketServer } from "ws";
+const wss = new WebSocketServer({ port: 8081 });
+let arr = [];
+wss.on("connection", function (socket) {
+    socket.on("message", function (message) {
+        //@ts-ignore
+        const parsedMessage = JSON.parse(message);
+        if (parsedMessage.type == "join") {
+            arr.push({
+                socket,
+                room_id: parsedMessage.payload.room_id
+            });
+        }
+        if (parsedMessage.type = "chat") {
+            let currentRoom;
+            for (let i = 0; i < arr.length; i++) {
+                if (arr[i]?.socket == socket) {
+                    currentRoom = arr[i]?.room_id;
+                }
+            }
+            for (let i = 0; i < arr.length; i++) {
+                if (arr[i]?.room_id == currentRoom) {
+                    arr[i]?.socket.send(message + "ssssss");
+                }
+            }
+        }
+    });
+});
+//# sourceMappingURL=socket.js.map
---
+++ src/socket.ts
@@ -0,0 +1,43 @@
+import { WebSocketServer } from "ws";
+const wss = new WebSocketServer({port:8081});
+
+
+let arr = [];
+
+wss.on("connection",function(socket){
+
+
+  socket.on("message",function(message){
+
+    //@ts-ignore
+    const parsedMessage = JSON.parse(message);
+    
+    if(parsedMessage.type == "join"){
+        arr.push({
+          socket,
+          room_id : parsedMessage.payload.room_id
+        })
+    }
+
+    if(parsedMessage.type = "chat"){
+
+      let currentRoom ;
+      
+      for(let i=0; i<arr.length ; i++){
+        if(arr[i]?.socket == socket){
+          currentRoom = arr[i]?.room_id;
+        }
+      }
+
+      for(let i=0; i<arr.length ; i++){
+        if(arr[i]?.room_id == currentRoom){
+          arr[i]?.socket.send(parsedMessage.payload.message+"ssssss");
+        }
+      }
+    }
+
+
+  })
+
+
+})
